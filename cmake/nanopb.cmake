21c1149b07e1db2bab4013811cf1f98150e36089
Jan Veverak Koniarik <squirrelcze@gmail.com>
2023-06-02 08:37:09 +0200

    added nanopb v1

---
+++ cmake/nanopb.cmake
@@ -0,0 +1,18 @@
+FetchContent_Declare(
+  nanopb
+  GIT_REPOSITORY https://github.com/nanopb/nanopb.git
+  GIT_TAG 0.4.7)
+FetchContent_GetProperties(nanopb)
+if(NOT nanopb_POPULATED)
+  FetchContent_Populate(nanopb)
+endif()
+
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${nanopb_SOURCE_DIR}/extra)
+
+find_package(Nanopb REQUIRED)
+
+add_library(
+  nanopb_lib STATIC
+  ${nanopb_SOURCE_DIR}/pb_common.c ${nanopb_SOURCE_DIR}/pb_encode.c
+  ${nanopb_SOURCE_DIR}/pb_decode.c)
+target_include_directories(nanopb_lib PUBLIC ${NANOPB_INCLUDE_DIRS})
