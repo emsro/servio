2e99b6ef9f31eb5e5bb9ae16ecef1bdbd99e24ef
Jan Veverak Koniarik <squirrelcze@gmail.com>
2023-11-10 20:18:55 +0100

    maded cmake utils into macros

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,4 +1,4 @@
-function(servio_add_library)
+macro(servio_add_library)
   cmake_parse_arguments(A "" "TARGET" "INCLUDE;SOURCES;LIBS;OPTS" ${ARGN})
 
   add_library(${A_TARGET} OBJECT ${A_SOURCES})
@@ -7,9 +7,9 @@ function(servio_add_library)
   target_link_libraries(${A_TARGET} PUBLIC ${A_LIBS})
   target_compile_options(${A_TARGET} PRIVATE ${A_OPTS})
 
-endfunction()
+endmacro()
 
-function(servio_add_board_executable)
+macro(servio_add_board_executable)
   cmake_parse_arguments(A "" "TARGET;LINKER_SCRIPT" "INCLUDE;SOURCES;LIBS;OPTS"
                         ${ARGN})
 
@@ -21,9 +21,9 @@ function(servio_add_board_executable)
   target_link_options(${A_TARGET} PRIVATE -Wl,--print-memory-usage)
 
   stm32_add_linker_script(${A_TARGET} PRIVATE ${A_LINKER_SCRIPT})
-endfunction()
+endmacro()
 
-function(servio_add_test)
+macro(servio_add_test)
   cmake_parse_arguments(A "" "TARGET" "INCLUDE;SOURCES;LIBS;" ${ARGN})
   add_executable(${A_TARGET} ${A_SOURCES})
   target_include_directories(${A_TARGET} PUBLIC ${A_INCLUDE})
@@ -32,4 +32,4 @@ function(servio_add_test)
   servio_compile_options(${A_TARGET})
 
   add_test(NAME ${A_TARGET}_test COMMAND ${A_TARGET})
-endfunction()
+endmacro()
