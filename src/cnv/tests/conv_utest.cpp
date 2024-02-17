b37679f79c46d55746570bc5cbe5832f37da34df
Jan Veverak Koniarik <squirrelcze@gmail.com>
2024-02-17 09:23:06 +0100

    improved linear test

--- src/cnv/tests/conv_utest.cpp
+++ src/cnv/tests/conv_utest.cpp
@@ -1,5 +1,6 @@
 #include "cnv/linear_converter.hpp"
 
+#include <emlabcpp/algorithm.h>
 #include <emlabcpp/range.h>
 #include <gtest/gtest.h>
 #include <random>
@@ -9,17 +10,28 @@ namespace servio::cnv::tests
 
 namespace em = emlabcpp;
 
-static constexpr std::size_t ATTEMPT_N = 100;
+static constexpr uint32_t ATTEMPT_N = 100;
 
 TEST( cnv, linear )
 {
-        std::default_random_engine                e( 0 );
-        std::binomial_distribution< std::size_t > bd( ATTEMPT_N, 0.5F );
+        std::default_random_engine             e( 0 );
+        std::binomial_distribution< uint32_t > bd( ATTEMPT_N, 0.5F );
 
         linear_converter lc{ .offset = 0, .scale = 0 };
 
-        for ( auto i : em::range( ATTEMPT_N ) )
-                EXPECT_EQ( lc.convert( bd( e ) ), 0.F );
+        for ( float s : { 0.0F, -1.0F, 1.0F, 3.1415F } )
+                for ( float o : { 0.0F, -1.0F, 1.0F, 3.1415F } ) {
+                        lc.offset = o;
+                        lc.scale  = s;
+                        for ( auto i : em::range( ATTEMPT_N ) ) {
+                                std::ignore = i;
+
+                                uint32_t v = bd( e );
+                                float    expected =
+                                    em::map_range( v, 0U, ATTEMPT_N, o, o + ATTEMPT_N * s );
+                                EXPECT_FLOAT_EQ( lc.convert( v ), expected );
+                        }
+                }
 }
 
 }  // namespace servio::cnv::tests
