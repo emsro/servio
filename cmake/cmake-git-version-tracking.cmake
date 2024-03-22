06c7250971f6ca216be3ef8d14d2164b0183b89c
Jan Veverak Koniarik <squirrelcze@gmail.com>
2024-03-22 18:26:54 +0100

    moved to submodules

--- cmake/cmake-git-version-tracking.cmake
+++
@@ -1,6 +0,0 @@
-FetchContent_Declare(
-  cmake_git_version_tracking
-  GIT_REPOSITORY https://github.com/andrew-hardin/cmake-git-version-tracking.git
-  GIT_TAG 6c0cb87
-  GIT_SHALLOW TRUE)
-FetchContent_MakeAvailable(cmake_git_version_tracking)
--- cmake/nanopb.cmake
+++ cmake/nanopb.cmake
@@ -1,22 +1,9 @@
-include(FetchContent)
-
-FetchContent_Declare(
-  nanopb
-  GIT_REPOSITORY https://github.com/nanopb/nanopb.git
-  GIT_TAG 0.4.7
-  GIT_SHALLOW TRUE
-  GIT_PROGRESS TRUE)
-FetchContent_GetProperties(nanopb)
-if(NOT nanopb_POPULATED)
-  FetchContent_Populate(nanopb)
-endif()
-
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${nanopb_SOURCE_DIR}/extra)
+set(NANOPB_DIR ${CMAKE_SOURCE_DIR}/dep/nanopb/)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${NANOPB_DIR}/extra)
 
 find_package(Nanopb REQUIRED)
 
 add_library(
-  nanopb_lib STATIC
-  ${nanopb_SOURCE_DIR}/pb_common.c ${nanopb_SOURCE_DIR}/pb_encode.c
-  ${nanopb_SOURCE_DIR}/pb_decode.c)
-target_include_directories(nanopb_lib SYSTEM PUBLIC ${NANOPB_INCLUDE_DIRS})
+  nanopb_lib STATIC ${NANOPB_DIR}/pb_common.c ${NANOPB_DIR}/pb_encode.c
+                    ${NANOPB_DIR}/pb_decode.c)
+target_include_directories(nanopb_lib SYSTEM PUBLIC ${NANOPB_DIR})
