325cef5978a0438a875ef2b596a41b6629f2765f
koniarik <5853442+koniarik@users.noreply.github.com>
2025-03-29 10:31:52 +0000

    Deploying to gh-pages from @ emsro/servio@91a3fb674c9e223f1d719d35503e5ff9106f95a7 🚀

--- structservio_1_1scmdio_1_1stream__iface.js
+++ structservio_1_1scmdio_1_1stream__iface.js
@@ -1,8 +1,8 @@
 var structservio_1_1scmdio_1_1stream__iface =
 [
     [ "~stream_iface", "structservio_1_1scmdio_1_1stream__iface.html#ae5b08878646ea1ef44a48ce2c1849809", null ],
-    [ "read", "structservio_1_1scmdio_1_1stream__iface.html#aa58149600161ab2ef8be367adbfe5aac", null ],
+    [ "read", "structservio_1_1scmdio_1_1stream__iface.html#a71fdb22e45057da31c472af39b79f190", null ],
     [ "read", "structservio_1_1scmdio_1_1stream__iface.html#abfe801fedb9916cd12ad28a4613dc879", null ],
-    [ "write", "structservio_1_1scmdio_1_1stream__iface.html#a64b9134dbe179566b41f10bdc3a0ccc9", null ],
+    [ "write", "structservio_1_1scmdio_1_1stream__iface.html#aea331b1d7fe68344885c89bf778196e3", null ],
     [ "write", "structservio_1_1scmdio_1_1stream__iface.html#a2b87212d9aea3daf2b643f5fb0924c7e", null ]
 ];
