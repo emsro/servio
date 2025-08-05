1a13d48c056f73024ca228876e3b6a11e2a2568e
Jan Veverak Koniarik <squirrelcze@gmail.com>
2025-08-05 07:45:52 +0200

    moved on_cmd

--- src/gov/governor.hpp
+++ src/gov/governor.hpp
@@ -15,8 +15,6 @@ namespace servio::gov
 
 struct handle
 {
-        virtual status on_cmd( iface::cmd_parser cmd, servio::iface::root_ser out ) = 0;
-
         virtual pwr current_irq( microseconds now, float current ) = 0;
 
         virtual void
@@ -33,10 +31,11 @@ struct engage_res
 
 struct governor : zll::ll_base< governor >
 {
-        virtual cfg::iface&      get_cfg()                               = 0;
-        virtual std::string_view name() const                            = 0;
-        virtual engage_res       engage( std::span< std::byte > buffer ) = 0;
-        virtual status           disengage( handle& )                    = 0;
+        virtual cfg::iface&      get_cfg()                                                    = 0;
+        virtual std::string_view name() const                                                 = 0;
+        virtual status           on_cmd( iface::cmd_parser cmd, servio::iface::root_ser out ) = 0;
+        virtual engage_res       engage( std::span< std::byte > buffer )                      = 0;
+        virtual status           disengage( handle& )                                         = 0;
 };
 
 governor* find_governor( std::string_view name );
